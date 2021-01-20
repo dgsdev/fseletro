615be81863a0511f9a7ef5a0813e19709b329b1b
dgsdev <dgspramos@gmail.com>
2021-01-20 19:26:16 -0300

    Ajuste Mysql 0.3

--- src/backend/link.php
+++ src/backend/link.php
@@ -1,15 +1,17 @@
 <?php
 
+$servername = "sql102.epizy.com";
+$username = "epiz_26883339";
+$password = "tzXyEHZIDQTDeW3";
+$database = "epiz_26883339_jump";
+
 class Connection
 {
     public static function getDb()
     {
         $conn = new PDO(
-            "mysql:host=sql102.epizy.com;dbname=epiz_26883339_projetorecode;charset=utf8",
-            "epiz_26883339",
-            "tzXyEHZIDQTDeW3"
-        );
-
+            "mysql:host=67.205.138.239:3306;dbname=fseletro","dgsdev","@dgsjuh12");        
+       
         if ($conn) {
             return $conn;
             
